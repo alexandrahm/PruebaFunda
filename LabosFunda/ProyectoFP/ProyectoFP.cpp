759ff471ba0d76b64de0c2df3e5f9dad53fd0e71
alexandrahm <00027117@uca.edu.sv>
2020-07-10 00:36:47 -0600

    Proyecto 2

--- LabosFunda/ProyectoFP/ProyectoFP.cpp
+++ LabosFunda/ProyectoFP/ProyectoFP.cpp
@@ -1,70 +1,44 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void Entrada(costoPorArticulo Articulos[], int Articulo);
-void Salida(costoPorArticulo Articulos[], int n);
 const int longCad =20;
 struct costoPorArticulo{
-    char nombreArticul[longCad+1];
+    string nombreArticul;
     int cantidad;
     float precio;
     float costoPorArticulo;
-};
+} Articulos[20];
+void entrada(costoPorArticulo Articulos[], int Articulo);
+void costo(costoPorArticulo Articulos[], int n);
+void salida(costoPorArticulo Articulos[], int n);
+void total(costoPorArticulo Articulos[],int n);
 int main (){
-cout<<"Tenemos disponible los siguientes articulos: "<<endl<<"Pan, manzanas, leche, pastas, aceite, huevos, fresas, tomates, cebolla, chile, cafe, peras y queso"<<endl;
 int n, opcion;
 cout<<"Ingrese la cantidad de articulos diferentes: "<<endl;
 cin>>n;
-
-Articulos[n];
-    cin.ignore();
-        cout << "Opciones del articulo " << i+1 << endl;
-        cout << "1.Datos del articulo" << endl;
-        cout << "2.Calcular costo por el articulo" << endl;
-        cout << "3.Mostrando informacion del articulo" << endl;
-        if(n>1){
-        cout << "4.Siguiente articulo" << endl;
-        }
-        cout << "5.Salir";
-        cin >> opcion;
-    while(opcion!=5){
-        switch(opcion){
-            case 1: 
-                Entrada(Articulos,i);
-            break;
-            case 3:
-                Salida(Articulos,n);
-            break;
-            case 4:
-                i++;
-            break;
-            case 5:
-                i = 5;
-            break;
-
-        }
-
-    }
+    entrada(Articulos, n);
+    costo(Articulos, n);
+    salida(Articulos, n);
+    total(Articulos, n);
     return 0;
 }
-
-void Entrada(costoPorArticulo Articulos[], int Articulo){
-    string Nombre;
-    int Unidades;
-    float Precio;
-    cout << "Nombre del articulo" << endl;
-    getline(cin,Nombre,"\n");
-    cout << "Cantidad de unidades" << endl;
-    getline(cin,Unidades,"\n");
-    cout << "Precio de la unidad" << endl;
-    getline(cin,Precio,"\n");
-    Articulos[Articulo].nombreArticul = Nombre;
-    Articulos[Articulo].cantidad = Unidades;
-    Articulos[Articulo].precio = Precio;
-
+void entrada(costoPorArticulo Articulos[], int n){
+     //costoPorArticulo Articulos;
+     for(int i=0; i<n;i++){
+        float costo;
+        int cantidad;
+        cout<<"Ingrese nombre del articulo: ";
+        //getline (cin,Articulos[i].nombreArticul);
+        cin>>Articulos[i].nombreArticul;
+        cout<<"Ingrese precio unitario: ";
+        cin>>Articulos[i].costoPorArticulo;
+        cout<<"Ingrese la cantidad: ";
+        //getline (cin,Articulos[i].cantidad);
+        cin>>Articulos[i].cantidad;
+        Articulos[i].precio=Articulos[i].cantidad*Articulos[i].costoPorArticulo;
+    }
 }
-
-void Salida(costoPorArticulo Articulos[], int n){
+void salida(costoPorArticulo Articulos[], int n){
     for(int i=0;i<n;i++){
         cout << "Articulo" << i+1 << endl;
         cout << "Nombre del articulo: " << Articulos[i].nombreArticul << endl;
@@ -72,4 +46,15 @@ void Salida(costoPorArticulo Articulos[], int n){
         cout << "Precio de la unidad: " << Articulos[i].precio << endl;
     }
 }
+void costo(costoPorArticulo Articulos[], int n){
+    for(int i=0; i<n; i++){
+        cout<<Articulos[i].nombreArticul<<" precio: "<<Articulos[i].precio<<endl;
+    }
+}
+void total(costoPorArticulo Articulos[],int n){
+    float total;
+    for(int i=0; i<n; i++){
+        total+=Articulos[i].precio;
+    }
+    cout<<"su total es: "<<total<<endl;
 }
